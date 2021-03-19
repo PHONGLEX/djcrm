bd412b64680c1a7a5585efaf8e20774a76c591f5
DESKTOP-6BCKNSI\DEV - F1 <phong.lx@citigo.com.vn>
2021-03-19 13:30:37 +0700

    - add templates folder to the root directory - add some other view and create lead form

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Lead
+from .forms import LeadForm
 
-# Create your views here.
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        'leads': leads
+    }
+    return render(request, 'leads/lead_list.html', context)
+
+
+def lead_detail(request, pk):
+    print(pk)
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead': lead
+    }
+    return render(request, 'leads/lead_detail.html', context)
+
+
+def lead_create(request):
+    context = {
+        "form": LeadForm()
+    }
+    return render(request, 'leads/lead_create.html', context)
